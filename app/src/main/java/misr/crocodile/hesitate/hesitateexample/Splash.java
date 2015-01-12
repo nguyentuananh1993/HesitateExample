8a376075f44557cb05dc1f7ce63021694e09d0d8
nguyentuananh1993 <nguyentuananh1993@gmail.com>
2015-01-12 09:08:11 +0700

    add music splash screen

--- app/src/main/java/misr/crocodile/hesitate/hesitateexample/Splash.java
+++ app/src/main/java/misr/crocodile/hesitate/hesitateexample/Splash.java
@@ -2,16 +2,20 @@ package misr.crocodile.hesitate.hesitateexample;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 /**
  * Created by misrc_000 on 1/11/15.
  */
 public class Splash extends Activity {
+    private MediaPlayer ourSong;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ourSong = MediaPlayer.create(Splash.this,R.raw.splash_sound);
         setContentView(R.layout.splash);
+        ourSong.start();
         Thread timer = new Thread(){
             public void run(){
                 try{
@@ -25,5 +29,12 @@ public class Splash extends Activity {
             }
         };
         timer.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        ourSong.stop();
+        finish();
     }
 }
